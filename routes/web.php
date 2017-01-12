75650b016d7ab537333dcc4c1eae539af7832b7c
adrDBh <adr.100@hotmail.com>
2017-01-11 19:57:52 -0600

    Fixed routes file.

--- routes/web.php
+++ routes/web.php
@@ -2,44 +2,7 @@
 
 // Routes
 Route::get('/', 'HomeController@Index');
-<<<<<<< HEAD
 Route::get('/home', 'HomeController@index');
 Route::post('/login','Auth\LoginController@login');
 Auth::routes();
-=======
-
-// Temporary route to insert an user and display it.
-Route::get('users', function(){
-	// Create a new User
-	// First way
-	/*try {
-		$user = new User();
-        $user->name = 'admin';
-        $user->password = bcrypt('admin');
-        $user->email = 'admin@bienestarcusur.com';
-        print_r($user);
-        $user->save();
-        echo 'saved\n';
-        return 'success';
-       }
-       catch(Exception $e){
-           return 'Could not create user';
-       }*/
-       // Second way
-       try{
-       	/*
-		User::create([
-       	'name' => 'admin',
-       	'email' => 'admin@bienestarcusur.com',
-       	'password' => bcrypt('admin')
-       	]);
-       	*/
-       return User::all();
-       } catch (Exception $e){
-       	return 'Could not create user.';
-       }     	
-});
-
-Auth::routes();
 Route::get('/home', 'HomeController@index');
->>>>>>> parent of 9ec77a6... DB now has seeds, changes to front-end and still ongoing multiple-way user auth..
